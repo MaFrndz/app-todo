6d2f450f96cba1a6fb376213670267bf3bd2829d
Fernandez Altamirano, Miguel Angel <mafaltamirano@indracompany.com>
2025-05-27 18:10:01 -0500

    login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import Aura from '@primeng/themes/aura';
+import { providePrimeNG } from 'primeng/config';
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideClientHydration(),
+    providePrimeNG({
+      theme: {
+        preset: Aura,
+      },
+    })
+  ]
 };
