dd7c2caecab17ddd5f89285e40348f0ef49ddda0
Fernandez Altamirano, Miguel Angel <mafaltamirano@indracompany.com>
2025-05-27 17:43:17 -0500

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,9 +1,17 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
-  selector: 'app-login',
-  standalone: true,
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    imports: [
+        ButtonModule,
+        InputTextModule,
+        FormsModule
+    ],
+    standalone: true,
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {}
+export class LoginComponent { }
