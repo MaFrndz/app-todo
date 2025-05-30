46472782d405241da1debeb9474774a1e503a2cb
Fernandez Altamirano, Miguel Angel <mafernandeza@unc.edu.pe>
2025-05-30 11:19:00 -0500

    fila de estados,

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { DragDropModule } from 'primeng/dragdrop';
 import { State } from '../../models/state/state.model';
@@ -9,7 +9,8 @@ import { StateService } from '../../services/state.service';
 export interface Product {
   id: string;
   titulo: string;
-  descripcion: string
+  descripcion: string;
+  idState: number;
 }
 
 @Component({
@@ -24,47 +25,47 @@ export interface Product {
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
-  stateService = inject(StateService);
-
-  availableProducts: Product[] = [];
-  selectedProducts: Product[] = [];
-  draggedProduct: Product | null = null;
   states: State[] = [];
+  tasksByState: { [idState: number]: Product[] } = {};
+  draggedTask: Product | null = null;
 
+  constructor(private readonly stateService: StateService) {}
 
   ngOnInit() {
-    this.selectedProducts = [];
-    this.availableProducts = [
-      { id: '1', titulo: 'Tarea 1', descripcion: 'Descripcion tarea 1' },
-      { id: '2', titulo: 'Tarea 2', descripcion: 'Descripcion tarea 2' },
-    ];
-    this.stateService.getStates().subscribe(
-      states => {
+    this.stateService.getStates().subscribe(states => {
       this.states = states;
+      // Inicializar tareas de ejemplo por estado
+      for (const state of states) {
+        this.tasksByState[state.idState] = [];
+      }
+      // Ejemplo: asignar tareas a estados
+      this.tasksByState[states[0]?.idState]?.push({
+        id: '1', titulo: 'Tarea 1', descripcion: 'Descripcion tarea 1', idState: states[0]?.idState
+      });
+      this.tasksByState[states[1]?.idState]?.push({
+        id: '2', titulo: 'Tarea 2', descripcion: 'Descripcion tarea 2', idState: states[1]?.idState
+      });
     });
   }
 
-  dragStart(product: Product) {
-    this.draggedProduct = product;
+  dragStart(task: Product) {
+    this.draggedTask = task;
   }
 
-  drop() {
-    if (this.draggedProduct) {
-      const draggedProductIndex = this.findIndex(this.draggedProduct);
-      if (draggedProductIndex > -1) {
-        this.selectedProducts = [...this.selectedProducts, this.draggedProduct];
-        this.availableProducts = this.availableProducts.filter((_, i) => i !== draggedProductIndex);
-      }
-      this.draggedProduct = null;
+  drop(targetState: State) {
+    if (this.draggedTask && this.draggedTask.idState !== targetState.idState) {
+      // Remover de la lista de origen
+      const originStateId = this.draggedTask.idState;
+      this.tasksByState[originStateId] = this.tasksByState[originStateId].filter(t => t.id !== this.draggedTask!.id);
+      // Cambiar el estado de la tarea
+      this.draggedTask.idState = targetState.idState;
+      // Agregar a la lista de destino
+      this.tasksByState[targetState.idState].push(this.draggedTask);
+      this.draggedTask = null;
     }
   }
 
   dragEnd() {
-    this.draggedProduct = null;
-  }
-
-  findIndex(product: Product): number {
-    return this.availableProducts.findIndex(p => p.id === product.id);
+    this.draggedTask = null;
   }
 }
